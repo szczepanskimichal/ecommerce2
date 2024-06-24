ff1bf0999054fcc73316b2cfa00b061519c52ed6
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-06-24 21:08:34 +0200

    Add env ti gitingore, created component Layout, install, next-auth, Update _app.js to use SessionProvider and Layout

---
+++ comonents/Layout.js
@@ -0,0 +1,46 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import { FaFacebookSquare, FaApple } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+// import Nav from "./Nav";
+// import MobileNav from "./MobileNav";
+
+export default function Layout({ children }) {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-gray-300 w-screen h-screen flex items-center justify-center">
+        <div className="container flex flex-col gap-3 max-w-[25rem]">
+          <h1 className="text-center">Log In</h1>
+          <button className="bg-white flex items-center justify-center gap-2">
+            <FaFacebookSquare className="size-6 text-blue-900" />
+            Use Facebook
+          </button>
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white flex items-center justify-center gap-2"
+          >
+            <FcGoogle className="size-6" />
+            Use Google
+          </button>
+          <button className="bg-white flex items-center justify-center gap-2">
+            <FaApple className="size-6" />
+            Use Apple
+          </button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="realative">
+      <div className="bg-gray-200 flex min-h-screen">
+        {/* <Nav /> */}
+        <div className="bg-gray-50 shadow-xl flex-grow sm:m-5 ml-0 sm:rounded-lg p-3">
+          {children}
+        </div>
+      </div>
+      <div className="sm:hidden absolute top-0 right-0 h-screen">
+        {/* <MobileNav /> */}
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return <div>HOME</div>;
+  const { data: session } = useSession();
+  console.log(session?.user);
+  return (
+    <div className="text-primary flex items-center justify-between sm:mr-0 mr-[70px]">
+      Hello, {session?.user?.name}
+      <img
+        className="size-12 object-cover rounded-full"
+        src={session?.user?.image}
+        alt=""
+      />
+    </div>
+  );
 }
