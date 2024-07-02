49f418f7b02b84314aec98864196a47aefda60a5
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-02 14:24:19 +0200

    Add initial ProductForm component
    - Created ProductForm component using React and useState
    - Initialized images state with an empty array
    - Added form with the following elements:
      - Title input field
      - Category select dropdown
      - Photos section with a message displayed when there are no photos

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,3 +1,17 @@
+import React, { useState } from "react";
+
 export default function ProductForm() {
-  return <div>Product Form</div>;
+  const [images, setImages] = useState([]);
+  return (
+    <form>
+      <label>Title</label>
+      <input type="text" name="Title" />
+      <label>Category</label>
+      <select></select>
+      <label>Photos</label>
+      {!images?.length && (
+        <div className="text-slate-600">No photos for this product.</div>
+      )}
+    </form>
+  );
 }
