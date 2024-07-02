3bb7f257df27cf1320157103b1bc58d06dce5b79
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-02 14:25:49 +0200

    Add Product model schema
    - Imported necessary modules from mongoose and mongodb
    - Defined ProductSchema with the following fields:
      - title: String, required
      - description: String
      - price: Number, required
      - images: Array of Strings
      - category: ObjectId, reference to Category model
      - properties: Object
    - Added timestamps option to schema for createdAt and updatedAt fields
    - Exported Product model, using existing model if it already exists or creating a new one

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+import { Timestamp } from "mongodb";
+import mongoose, { Schema, model, models } from "mongoose";
+import { type } from "os";
+
+const ProductSchema = new Schema(
+  {
+    tittle: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    properties: { type: Object },
+  },
+  {
+    timestamps: true,
+  }
+);
+export default Product = models.Product || model("Product", ProductSchema);
