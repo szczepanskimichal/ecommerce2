e05727df933bba2e32d4af746a8f3889d25dac0c
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-05 13:37:42 +0200

    Update categories.js
    "Add category management functionality to Categories component
    - Import useState, useEffect from React and axios for API requests.
    - Manage state for categories, editedCategory, name, parentCategory, and properties.
    - Implement saveCategory function for adding and updating categories.
    - Add functions to manage properties: addProperty, removeProperty, handlePropertyNameChange, handlePropertyValuesChange.
    - Create form to add and edit categories and properties.
    - Add buttons for saving, canceling, and removing properties.
    "

--- pages/categories.js
+++ pages/categories.js
@@ -1,10 +1,151 @@
 import { withSwal } from "react-sweetalert2";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function Categories() {
+function Categories({ swal }) {
+  const [editedCategory, setEditedCategory] = useState(null);
+  const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [parentCategory, setParentCategory] = useState("");
+  const [properties, setProperties] = useState([]);
+
+  async function saveCategory(e) {
+    e.preventDefault();
+    const data = {
+      name,
+      parentCategory,
+      properties: properties.map((p) => ({
+        name: p.name,
+        values: p.values.split(","),
+      })),
+    };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
+    setName("");
+    setParentCategory("");
+    setProperties([]);
+  }
+
+  function addProperty() {
+    setProperties((prev) => {
+      return [...prev, { name: "", values: "" }];
+    });
+  }
+
+  function removeProperty(indexToRemove) {
+    setProperties((prev) => {
+      return [...prev].filter((property, index) => {
+        return index !== indexToRemove;
+      });
+    });
+  }
+
+  function handlePropertyNameChange(index, newName) {
+    setProperties((prev) => {
+      const properties = [...prev];
+      properties[index].name = newName;
+      return properties;
+    });
+  }
+
+  function handlePropertyValuesChange(index, newValues) {
+    setProperties((prev) => {
+      const properties = [...prev];
+      properties[index].values = newValues;
+      return properties;
+    });
+  }
   return (
-    <div>
-      <h1>Categories({swal})</h1>
-    </div>
+    <>
+      <h1>Categories</h1>
+      <form onSubmit={saveCategory}>
+        <label>
+          {editedCategory
+            ? `Edit category ${editedCategory.name}`
+            : "New category name"}
+        </label>
+        <div className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Category name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <select
+            value={parentCategory}
+            onChange={(e) => setParentCategory(e.target.value)}
+          >
+            <option value="">No parent category</option>
+            {categories.length > 0 &&
+              categories.map((category) => (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="flex flex-col items-start gap-2 mb-3">
+          <label className="block">Properties</label>
+          <button onClick={addProperty} type="button" className="btn-secondary">
+            Add new property
+          </button>
+          {properties.length > 0 &&
+            properties.map((property, index) => (
+              <div key={index} className="flex gap-2">
+                <input
+                  type="text"
+                  className="mb-0"
+                  value={property.name}
+                  onChange={(e) =>
+                    handlePropertyNameChange(index, e.target.value)
+                  }
+                  placeholder="Property name"
+                />
+                <input
+                  type="text"
+                  className="mb-0"
+                  value={property.values}
+                  onChange={(e) =>
+                    handlePropertyValuesChange(index, e.target.value)
+                  }
+                  placeholder="Values, comma separated"
+                />
+                <button
+                  onClick={() => removeProperty(index)}
+                  type="button"
+                  className="btn-delete"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+        </div>
+        <div className="flex gap-2">
+          <button type="submit" className="btn-primary">
+            Save
+          </button>
+          {editedCategory && (
+            <button
+              type="button"
+              className="btn-secondary"
+              onClick={() => {
+                setEditedCategory(null);
+                setName("");
+                setParentCategory("");
+                setProperties([]);
+              }}
+            >
+              Cancel
+            </button>
+          )}
+        </div>
+      </form>
+    </>
   );
 }
 
